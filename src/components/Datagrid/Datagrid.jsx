f35b71d7e4085292ce3c3580c31cd960b11c2cfd
Hariharan <hariharan9876@gmail.com>
2022-06-17 11:07:30 +0530

    Added delete to datagrid component

--- src/components/Datagrid/Datagrid.jsx
+++ src/components/Datagrid/Datagrid.jsx
@@ -7,14 +7,39 @@ const Datagrid = () => {
   const [tableData, setTableData] = useState(userData);
   const [pageSize, setPageSize] = useState(5);
 
-  
+  const handleDelete = (id) => {
+    setTableData(tableData.filter((item) => item.id !== id));
+  };
+
+  const actionColumns = [
+    {
+      field: "action",
+      headerName: "Action",
+      width: 200,
+      renderCell: (params) => {
+        return (
+          <>
+            <div className="action-cell">
+              <div className="action-button-view">View</div>
+              <div
+                className="action-button-delete" 
+                onClick={() => handleDelete(params.row.id)}
+              >
+                Delete
+              </div>
+            </div>
+          </>
+        )
+      }
+    }
+  ]
 
   return (
     <div className="data-grid-container">
       <div className="data-grid-title">Users</div>
       <DataGrid 
         rows={tableData}
-        columns={columns}
+        columns={columns.concat(actionColumns)}
         pageSize={pageSize}
         rowsPerPageOptions={[1, 5, 10, 25]}
         onPageSizeChange={(event) => {setPageSize(event)}}
