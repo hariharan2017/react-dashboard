76772a477a1b48472653260abb8660eceffff792
Hariharan <hariharan9876@gmail.com>
2022-06-17 10:55:51 +0530

    Implemented MUI Data Table

--- src/components/Datagrid/Datagrid.jsx
+++ src/components/Datagrid/Datagrid.jsx
@@ -1,11 +1,28 @@
 import "./Datagrid.scss";
+import { useState } from "react";
+import { columns, userData } from "./config";
+import { DataGrid } from '@mui/x-data-grid';
 
 const Datagrid = () => {
+  const [tableData, setTableData] = useState(userData);
+  const [pageSize, setPageSize] = useState(5);
+
+  
+
   return (
-    <div>
-      Test
+    <div className="data-grid-container">
+      <div className="data-grid-title">Users</div>
+      <DataGrid 
+        rows={tableData}
+        columns={columns}
+        pageSize={pageSize}
+        rowsPerPageOptions={[1, 5, 10, 25]}
+        onPageSizeChange={(event) => {setPageSize(event)}}
+        checkboxSelection
+        disableSelectionOnClick
+      />
     </div>
   );
-}
- 
+};
+
 export default Datagrid;
